942ce148b1b51f2798b9e36c3c76748ccd020f4b
Rachel Johnson <rachelktyjohnson@gmail.com>
2021-07-15 21:36:47 +1000

    Add jobrole section

---
+++ js/script.js
@@ -0,0 +1,9 @@
+let other_job_role = document.getElementById('other-job-role');
+other_job_role.style.display="none";
+
+let job_role = document.getElementById('title');
+job_role.addEventListener('change', (e)=>{
+    if(e.target.value === 'other'){
+        other_job_role.style.display="block";
+    }
+})
